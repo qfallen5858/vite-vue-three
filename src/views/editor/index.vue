a192f4299fbc5b92d1c0b37b5289cf7cbbb437bb
tuotuo <qufan@home.com>
2022-07-19 00:41:18 +0800

    test

--- src/views/editor/index.vue
+++ src/views/editor/index.vue
@@ -1,30 +1,49 @@
 <template>
   <n-space vertical size="large">
     <n-layout has-sider>
-        <n-layout-sider collapse-mode="transform" :collapsed-width="120" :width="240" show-trigger="bar" content-style="padding:24px" bordered>
-            <n-collapse>
-                <n-collapse-item title="Edit FloorPlan" name="1">
-                    <div>test</div>
-                </n-collapse-item>
-                <n-collapse-item title="Design" name="2">
-                    <div>test</div>
-                </n-collapse-item>
-                <n-collapse-item title="Add Items" name="3">
-                    <div>test</div>
-                </n-collapse-item>
-            </n-collapse>
-        </n-layout-sider>
-        <n-layout-content content-style="padding:24px">
-            <div id="floorplanner">
-                <canvas id="floorplanner-canvas" ></canvas>
-            </div>
-        </n-layout-content>
+      <n-layout-sider collapse-mode="transform" :collapsed-width="120" :width="240" show-trigger="bar"
+        content-style="padding:24px" bordered>
+        <n-collapse>
+          <n-collapse-item title="Edit FloorPlan" name="1">
+            <div>test</div>
+          </n-collapse-item>
+          <n-collapse-item title="Design" name="2">
+            <div>test</div>
+          </n-collapse-item>
+          <n-collapse-item title="Add Items" name="3">
+            <div>test</div>
+          </n-collapse-item>
+        </n-collapse>
+      </n-layout-sider>
+      <n-layout-content content-style="padding:24px">
+        <div id="floorplanner">
+          <canvas id="floorplanner-canvas"></canvas>
+        </div>
+      </n-layout-content>
     </n-layout>
   </n-space>
 </template>
 
 <script setup lang="ts">
-import {NSpace, NLayout, NLayoutContent,NLayoutSider, NH2, NCollapse, NCollapseItem} from "naive-ui"
+import { onMounted, getCurrentInstance, ComponentInternalInstance } from "vue";
+import { NSpace, NLayout, NLayoutContent, NLayoutSider, NH2, NCollapse, NCollapseItem } from "naive-ui"
+
+const { proxy } = getCurrentInstance() as ComponentInternalInstance;
+
+const init = () => {
+  if (proxy != null) {
+    let canvasElement = proxy.$el.querySelector("#floorplanner-canvas");
+    let canvasWrapperEle = proxy.$el.querySelector("#floorplanner");
+    window.onresize = function () {
+      canvasWrapperEle.height = window.innerHeight - canvasWrapperEle.offsetTop;
+      canvasElement.height = window.innerHeight - canvasElement.offsetTop;
+    }
+  }
+
+}
+
+onMounted(init);
 </script>
 
-<style></style>
+<style>
+</style>
