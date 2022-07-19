586b4f6c0561b63c792f303b0d366fc681d57d8f
tuotuo <qufan@home.com>
2022-07-20 01:11:14 +0800

    grid ok

--- src/libs/floorplanner/floorplanner.ts
+++ src/libs/floorplanner/floorplanner.ts
@@ -1,19 +1,28 @@
-namespace BP3D {
+// export {};
+export namespace BP3D {
+  const gridSpacing: number = 20;
+  const gridWidth: number = 1;
+  const gridColor: string = "#f1f1f1";
+
   export class Floorplanner {
     private canvasElement: HTMLCanvasElement;
 
     private context: CanvasRenderingContext2D | null;
 
-    constructor(private cavasSelector: string) {
+    constructor(private canvasSelector: string) {
       this.canvasElement = document.getElementById(
-        cavasSelector
+        this.canvasSelector
       ) as HTMLCanvasElement;
-      this.context = this.canvasElement.getContext("2d");
-      let scope = this;
-      window.onresize = () => {
+      if (this.canvasElement) {
+        this.context = this.canvasElement.getContext("2d");
+        let scope = this;
+        window.onresize = () => {
+          scope.handleWindowResize();
+        };
         scope.handleWindowResize();
-      };
-      scope.handleWindowResize();
+      }else{
+        this.context = null;
+      }
     }
 
     public handleWindowResize() {
@@ -25,17 +34,57 @@ namespace BP3D {
       }
     }
 
-    public draw() {
-        if(this.context == null){
-            return;
-        }
-        this.context.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
-        this.drawGrid();
+    private calculateGridOffset(n: number): number {
+      if (n >= 0) {
+        return ((n + gridSpacing / 2.0) % gridSpacing) - gridSpacing / 2.0;
+      } else {
+        return ((n - gridSpacing / 2.0) % gridSpacing) + gridSpacing / 2.0;
+      }
     }
 
-    private drawGrid(){
-        
+    private drawLine(
+      startX: number,
+      startY: number,
+      endX: number,
+      endY: number,
+      width: number,
+      color: string
+    ): void {
+      if (this.context) {
+        this.context.beginPath();
+        this.context.moveTo(startX, startY);
+        this.context.lineTo(endX, endY);
+        this.context.lineWidth = width;
+        this.context.strokeStyle = color;
+        this.context.stroke();
+      }
+    }
+
+    public draw(): void {
+      if (this.context == null) {
+        return;
+      }
+      this.context.clearRect(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      this.drawGrid();
+    }
+
+    private drawGrid() {
+      let offsetX = this.calculateGridOffset(0);
+      let offsetY = this.calculateGridOffset(0);
+      let width = this.canvasElement.width;
+      let height = this.canvasElement.height;
+      for(let x = 0; x < (width / gridSpacing); x++){
+        this.drawLine(gridSpacing * x + offsetX, 0, gridSpacing * x + offsetX, height, gridWidth, gridColor)
+      }
+      for(let y = 0; y < (height / gridSpacing); y++){
+        this.drawLine(0, gridSpacing * y + offsetY, width, gridSpacing * y + offsetY, gridWidth, gridColor);
+      }
+      // this.drawLine(0, 0, 100, 100, 2, gridColor);
     }
   }
 }
-export {};
--- src/views/editor/index.vue
+++ src/views/editor/index.vue
@@ -15,7 +15,7 @@
           </n-collapse-item>
         </n-collapse>
       </n-layout-sider>
-      <n-layout-content content-style="padding:24px">
+      <n-layout-content content-style="padding:24px; height:100%">
         <div id="floorplanner">
           <canvas id="floorplanner-canvas"></canvas>
         </div>
@@ -27,17 +27,20 @@
 <script setup lang="ts">
 import { onMounted, getCurrentInstance, ComponentInternalInstance } from "vue";
 import { NSpace, NLayout, NLayoutContent, NLayoutSider, NH2, NCollapse, NCollapseItem } from "naive-ui"
-
+import {BP3D} from '../../libs/floorplanner/floorplanner'
 const { proxy } = getCurrentInstance() as ComponentInternalInstance;
 const _this = proxy;
 const init = () => {
-  if (proxy != null) {
-    let canvasElement = proxy.$el.querySelector("#floorplanner-canvas");
-    let canvasWrapperEle = proxy.$el.querySelector("#floorplanner");
+  if (_this != null) {
+    let canvasElement = _this.$el.querySelector("#floorplanner-canvas");
+    let canvasWrapperEle = _this.$el.querySelector("#floorplanner");
     window.onresize = function () {
+      console.log(window.innerHeight)
       canvasWrapperEle.height = window.innerHeight - canvasWrapperEle.offsetTop;
-      canvasElement.height = window.innerHeight - canvasElement.offsetTop;
+      // canvasElement.height = window.innerHeight - canvasElement.offsetTop;
+      // canvasElement.width = canvasWrapperEle.clientWidth;
     }
+    new BP3D.Floorplanner("floorplanner-canvas");
   }
 
 }
