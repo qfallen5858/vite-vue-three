5fc4bd505cd3de5586c540093be5c5df46e497d1
qufan <allen5858@163.com>
2022-07-22 00:01:38 +0800

    types defined

--- src/libs/design2d/designer.ts
+++ src/libs/design2d/designer.ts
@@ -1,93 +1,71 @@
-export namespace KQDesign {
-  
-  
-  
-  
-  const gridSpacing: number = 20;
-  const gridWidth: number = 1;
-  const gridColor: string = "#f1f1f1";
-  
-  export class Designer {
-    private canvasElement: HTMLCanvasElement;
+import { DesignerView, DesignMode } from "./designer_view";
+import { KQPosition } from "../core/types";
+const gridSpacing: number = 20;
+const gridWidth: number = 1;
+const gridColor: string = "#f1f1f1";
 
-    private context: CanvasRenderingContext2D | null;
+export class Designer {
+  public originPosition: KQPosition = { x: 0, y: 0 };
+  public originX: number = 0;
 
-    constructor(private canvasSelector: string) {
-      this.canvasElement = document.getElementById(
-        this.canvasSelector
-      ) as HTMLCanvasElement;
-      if (this.canvasElement) {
-        this.context = this.canvasElement.getContext("2d");
-        let scope = this;
-        window.onresize = () => {
-          scope.handleWindowResize();
-        };
-        scope.handleWindowResize();
-      }else{
-        this.context = null;
-      }
-    }
+  public originY: number = 0;
 
-    public handleWindowResize() {
-      let parent = this.canvasElement.parentElement;
-      if (parent) {
-        this.canvasElement.width = parent.clientWidth;
-        this.canvasElement.height = parent.clientHeight;
-        this.draw();
-      }
-    }
+  private view: DesignerView;
 
-    private calculateGridOffset(n: number): number {
-      if (n >= 0) {
-        return ((n + gridSpacing / 2.0) % gridSpacing) - gridSpacing / 2.0;
-      } else {
-        return ((n - gridSpacing / 2.0) % gridSpacing) + gridSpacing / 2.0;
-      }
-    }
+  private canvasElement: HTMLCanvasElement;
 
-    private drawLine(
-      startX: number,
-      startY: number,
-      endX: number,
-      endY: number,
-      width: number,
-      color: string
-    ): void {
-      if (this.context) {
-        this.context.beginPath();
-        this.context.moveTo(startX, startY);
-        this.context.lineTo(endX, endY);
-        this.context.lineWidth = width;
-        this.context.strokeStyle = color;
-        this.context.stroke();
-      }
-    }
+  private mode!: DesignMode;
 
-    public draw(): void {
-      if (this.context == null) {
-        return;
-      }
-      this.context.clearRect(
-        0,
-        0,
-        this.canvasElement.width,
-        this.canvasElement.height
-      );
-      this.drawGrid();
-    }
+  private isMouseDown: boolean = false;
 
-    private drawGrid() {
-      let offsetX = this.calculateGridOffset(0);
-      let offsetY = this.calculateGridOffset(0);
-      let width = this.canvasElement.width;
-      let height = this.canvasElement.height;
-      for(let x = 0; x < (width / gridSpacing); x++){
-        this.drawLine(gridSpacing * x + offsetX, 0, gridSpacing * x + offsetX, height, gridWidth, gridColor)
-      }
-      for(let y = 0; y < (height / gridSpacing); y++){
-        this.drawLine(0, gridSpacing * y + offsetY, width, gridSpacing * y + offsetY, gridWidth, gridColor);
-      }
-      // this.drawLine(0, 0, 100, 100, 2, gridColor);
+  private isMouseMoving: boolean = false;
+
+  // 上次鼠标点击的坐标
+  private lastMouseClickPosition: KQPosition = { x: 0, y: 0 };
+  private lastMouseClickX: number = 0;
+  private lastMouseClickY: number = 0;
+
+  constructor(private canvasSelector: string) {
+    this.canvasElement = document.getElementById(
+      canvasSelector
+    ) as HTMLCanvasElement;
+    this.view = new DesignerView(this, this.canvasSelector);
+    this.canvasElement.addEventListener("mousedown", (e) => {
+      this.mousedown(e);
+    });
+    this.canvasElement.addEventListener("mousemove", (e) => {
+      this.mousemove(e);
+    });
+    this.canvasElement.addEventListener("mouseup", (e) => {
+      this.mouseup(e);
+    });
+    this.canvasElement.addEventListener("mouseout", (e) => {
+      this.mouseout(e);
+    });
+    this.setMode(DesignMode.MOVE);
+    document.addEventListener("keyup", (e) => {
+      this.keyup(e);
+    });
+  }
+
+  private keyup(event: KeyboardEvent): void {
+    if (event.code === "Escape") {
+      this.setMode(DesignMode.MOVE);
     }
   }
+
+  public setMode(mode: DesignMode) {
+    this.mode = mode;
+  }
+
+  private mousedown(event: MouseEvent): void {}
+
+  private mousemove(event: MouseEvent): void {}
+
+  private mouseup(event: MouseEvent): void {
+    console.log("mouse up: " + event);
+    console.log(this);
+  }
+
+  private mouseout(event: MouseEvent): void {}
 }
