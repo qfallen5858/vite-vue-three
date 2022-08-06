dab2aabd3d858783c6da7ad855a2a08be7485499
tuotuo <qufan@home.com>
2022-08-07 00:05:41 +0800

    test

---
+++ src/libs/core/mouse_controller.ts
@@ -0,0 +1,17 @@
+export class MouseController {
+  private isMouseDown: boolean = false;
+
+  private isMouseMoved: boolean = false;
+
+  private lastMouseClickX: number = 0;
+
+  private lastMouseClickY: number = 0;
+
+  public mouseDown(){
+
+  }
+
+  public mouseMove(event: MouseEvent){
+    
+  }
+}
