0cc192b707e5c96aecd5f86edfb7aa66814434a2
kq_qufan <qufan@kqgeo.com>
2023-05-29 16:50:38 +0800

    main repair

--- src/libs/three/main.ts
+++ src/libs/three/main.ts
@@ -7,25 +7,44 @@ interface ThreeOptions{
   spinSpeed?: number;
   clickPan?: boolean;
   canMoveFixedItems?: boolean;
+  domSelector: string;
 }
 
 export class Main{
   private camera!: THREE.PerspectiveCamera;
   private renderer!:THREE.Renderer;
   private scene!:THREE.Scene;
+  
   private options: ThreeOptions = {
     resize:true,
     pushHref:false,
     spin:true,
     spinSpeed:.00002,
-    canMoveFixedItems:false
+    canMoveFixedItems:false,
+    domSelector:'#three-container'
   };
   constructor(){
-
+    this._init();
   }
 
   private _init(){
     // Three.ImageUtils.crossOrigin = ''
     this.scene = new THREE.Scene();
+    const domElement:HTMLElement = document.getElementById(this.options.domSelector) as HTMLElement;
+    if(domElement != null){
+      throw new Error('domElement is null or undefined');
+    }
+    this.camera = new THREE.PerspectiveCamera(45, 1, 1, 10000);
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      preserveDrawingBuffer:true
+    });
+  
+    this.renderer.autoClear = false;
+    this.renderer.shadowMapEnabled = true;
+    this.renderer.shadowMapSoft = true;
+    this.renderer.shadowMapType = THREE.PCFSoftShadowMap;
+
+
   }
 }
