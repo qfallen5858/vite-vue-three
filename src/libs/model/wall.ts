90bfe94570737fc98a9ed3e9824dd2a3b080bc3a
allen5858 <allen5858@163.com>
2023-06-27 09:42:47 +0800

    wall modify

--- src/libs/model/wall.ts
+++ src/libs/model/wall.ts
@@ -1,3 +1,5 @@
+import { Callbacker, CustomFunc } from '../core/callback';
+import { Utils } from '../core/utils';
 import { Corner } from './corner';
 import * as INNER_JSON from './json'
 const defaultWallTexture:INNER_JSON.Texture = {
@@ -9,6 +11,12 @@ export class Wall{
   private _id:string;
 
   private _start:Corner;
+
+  private _moveCallback:Callbacker = new Callbacker();
+
+  private _deletedCallback:Callbacker = new Callbacker();
+
+  private _actionCallback:Callbacker = new Callbacker();
 
   private _end:Corner;
   constructor(start:Corner, end:Corner){
@@ -26,6 +34,43 @@ export class Wall{
   }
   
   public remove():void{
-
+    this._start.detachWall(this);
+    this._end.detachWall(this);
+    this._deletedCallback.fire(this);
   }
+
+  public set start(corner:Corner){
+    this._start.detachWall(this);
+    corner.attachStart(this);
+    this._start = corner;
+    this.fireMoved();
+  }
+
+  public set end(corner:Corner){
+    this._end.detachWall(this);
+    corner.attachEnd(this);
+    this._end = corner;
+    this.fireMoved();
+  }
+
+  public fireOnMove(func:CustomFunc):void{
+    this._moveCallback.add(func)
+  }
+
+  public fireOnDelete(func:CustomFunc):void{
+    this._deletedCallback.add(func);
+  }
+
+  public fireOnAction(func:CustomFunc):void{
+    this._actionCallback.add(func);
+  }
+
+  public fireMoved():void{
+    this._moveCallback.fire();
+  }
+
+  public distanceFrom( point:{x:number, y:number}):number{
+    return Utils.pointDistanceFromLine(point.x, point.y, this._start.x, this._start.y, this._end.x, this._end.y)
+  }
+
 }
