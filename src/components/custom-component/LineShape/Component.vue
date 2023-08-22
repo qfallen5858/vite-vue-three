28735ca39266b547cf3703d3c1aa668de4d3a4b5
allen5858 <allen5858@163.com>
2023-08-22 15:15:04 +0800

    lineshape

---
+++ src/components/custom-component/LineShape/Component.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="line-shape">
+  </div>
+</template>
+<script lang="ts">
+import OnEvent from '../common/OnEvent.vue';
+import {defineComponent} from 'vue'
+export default defineComponent({
+  extends:OnEvent
+})
+</script>
+<style lang="scss" scoped>
+.line-shape{
+  width: 100%;
+  height: 100;
+  overflow: auto;
+}
+</style>
